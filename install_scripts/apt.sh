4054aec0d02b4936a68104e34f138b0c972f8367
Dale Alleshouse <dalealleshouse@gmail.com>
2016-11-03 19:47:53 -0400

    YouCompleteMe

--- install_scripts/apt.sh
+++ install_scripts/apt.sh
@@ -1,11 +1,34 @@
 #!/bin/bash
 
+# Mono
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+echo "deb http://download.mono-project.com/repo/debian wheezy main" | sudo tee /etc/apt/sources.list.d/mono-xamarin.list
+echo "deb http://download.mono-project.com/repo/debian wheezy-apache24-compat main" | sudo tee -a /etc/apt/sources.list.d/mono-xamarin.list
+
+# Node
+# curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.1/install.sh | bash
+
 apt-get update
+
 apt-get install -y \
 	cowsay \
 	vim \
+	vim-nox-py2 \
 	tmux \
 	xclip \
 	git \
 	openssh-server \
-	docker-compose
+	docker-compose \
+	mono-runtime \
+	mono-xbuild \
+	mono-complete \
+	nodejs \
+	build-essential \
+	cmake \
+	npm \
+	python-dev
+
+
+nvm install node
+npm install -g typescript
--- install_scripts/vim-plugins.sh
+++ install_scripts/vim-plugins.sh
@@ -3,3 +3,29 @@
 apt-vim install -y https://github.com/scrooloose/nerdtree.git
 apt-vim install -y https://github.com/tomasr/molokai.git
 apt-vim install -y https://github.com/jlanzarotta/bufexplorer.git
+apt-vim install -y https://github.com/scrooloose/syntastic.git 
+apt-vim install -y https://github.com/tpope/vim-dispatch.git
+apt-vim install -y https://github.com/tpope/vim-commentary.git
+apt-vin install -y https://github.com/ctrlpvim/ctrlp.vim.git
+apt-vim install -y https://github.com/OrangeT/vim-csharp.git
+apt-vim install -y https://github.com/SirVer/ultisnips.git
+
+################ OmniSharp ################
+apt-vim install -y https://github.com/OmniSharp/omnisharp-vim.git
+
+cd ~/.vim/bundle/omnisharp-vim
+git submodule update --init --recursive
+
+# cd ~/.vim/bundle/omnisharp-vim/server
+# xbuild
+
+cd ~/.vim/bundle/omnisharp-vim/omnisharp-roslyn
+./build.sh
+
+################ You Complete Me ################ 
+apt-vim install -y https://github.com/Valloric/YouCompleteMe.git
+
+cd ~/.vim/bundle/YouCompleteMe
+git submodule update --init --recursive
+
+./install.py --clang-completer --omnisharp-completer --tern-completer
