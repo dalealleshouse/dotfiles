ec1f294c2df8e53c4a4eef72d5caa5407eb7fea6
Dale Alleshouse <dalealleshouse@gmail.com>
2017-04-14 11:09:27 -0400

    fix git config

--- install_scripts/git-config.sh
+++ install_scripts/git-config.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+apt-get install git-all
+
 git config --global user.email "dalealleshouse@gmail.com"
 git config --global user.name "Dale Alleshouse"
 
@@ -8,7 +10,5 @@ ssh-keygen -t rsa -b 4096 -C "dalealleshouse@gmail.com"
 eval `ssh-agent -s`
 ssh-add
 
-xclip -sel clip < ~/.ssh/id_rsa.pub
-
-echo "The ssh key is on your clipboard. Add it to GitHub to complete setup"
+echo "Copy the contents of ~/.ssh/id_rsa.pub into a github ssh key"
 echo "https://help.github.com/articles/adding-a-new-ssh-key-to-your-github-account/"
