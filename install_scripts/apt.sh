13e8895fe6138ac485c572d7001ec539095629b7
Dale Alleshouse <dalealleshouse@gmail.com>
2017-04-15 10:31:16 -0400

    fixed node install

--- install_scripts/apt.sh
+++ install_scripts/apt.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # dotnet
-sh -c 'echo "deb [arch=amd64] https://apt-mo.trafficmanager.net/repos/dotnet-release/ trusty main" > /etc/apt/sources.list.d/dotnetdev.list'
+sh -c 'echo "deb [arch=amd64] https://apt-mo.trafficmanager.net/repos/dotnet-release/ xenial main" > /etc/apt/sources.list.d/dotnetdev.list'
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 417A0893
 
 # Node
-curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.1/install.sh | bash
 
 # vim-apt
 curl -sL https://raw.githubusercontent.com/egalpin/apt-vim/master/install.sh | sh
@@ -21,14 +21,12 @@ apt-get install -y \
     tmux \
     git \
     openssh-server \
-    nodejs \
     build-essential \
     cmake \
     python-dev \
-    dotnet-dev-1.0.0-preview2.1-003177
+    dotnet-dev-1.0.1
 
 
-nvm install node
 npm install -g typescript
 npm install -g typescript-formatter
 npm install -g tslint
