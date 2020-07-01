f075008a423bfdb9b504faed2be231cf378d2576
Dale Alleshouse <dalealleshouse@gmail.com>
2020-07-01 19:47:06 -0400

    WIP

--- install_scripts/vim-plugins.sh
+++ install_scripts/vim-plugins.sh
@@ -3,7 +3,7 @@
 apt-vim install -y https://github.com/scrooloose/nerdtree.git
 apt-vim install -y https://github.com/tomasr/molokai.git
 apt-vim install -y https://github.com/jlanzarotta/bufexplorer.git
-apt-vim install -y https://github.com/scrooloose/syntastic.git
+# apt-vim install -y https://github.com/scrooloose/syntastic.git
 # apt-vim install -y https://github.com/tpope/vim-dispatch.git
 apt-vim install -y https://github.com/tpope/vim-commentary.git
 apt-vim install -y https://github.com/ctrlpvim/ctrlp.vim.git
@@ -20,6 +20,7 @@ apt-vim install -y git://github.com/LaTeX-Box-Team/LaTeX-Box.git
 apt-vim install -y https://github.com/rhysd/vim-grammarous.git
 apt-vim install -y https://github.com/flazz/vim-colorschemes.git
 apt-vim install -y https://github.com/benshuailyu/online-thesaurus-vim.git
+apt-vim install -y https://github.com/dense-analysis/ale.git
 
 ################ python-mode ################
 apt-vim install -y https://github.com/python-mode/python-mode.git
