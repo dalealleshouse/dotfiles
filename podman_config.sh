20f45dc8fce42545c03483a56a647cb72b97bfa3
Dale Alleshouse <dale.alleshouse@gmail.com>
2023-03-29 10:37:10 -0400

    WIP

--- podman_config.sh
+++ podman_config.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
-
-echo "deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_20.04/ /" | sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list
-curl -L "https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_20.04/Release.key" | sudo apt-key add -
-
 grep -qxF "alias docker='podman'" ~/.bash_aliases || echo "alias docker='podman'" >> ~/.bash_aliases
 grep -qxF "alias docker-compose='podman-compose'" ~/.bash_aliases || echo "alias docker-compose='podman-compose'" >> ~/.bash_aliases
-
-#!/bin/bash
 
 CONFIG_DIR="$HOME/.config/containers"
 CONFIG_FILE="containers.conf"
